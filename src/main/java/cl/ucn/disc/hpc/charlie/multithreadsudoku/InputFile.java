31b3af541e1365de9e4abd69cf70e3f8b0ef2658
charlie <charliecondorcet@gmail.com>
2020-11-01 15:47:18 -0300

    feat(InputFile): add txt entry reader

--- src/main/java/cl/ucn/disc/hpc/charlie/multithreadsudoku/InputFile.java
+++ src/main/java/cl/ucn/disc/hpc/charlie/multithreadsudoku/InputFile.java
@@ -19,16 +19,45 @@
 
 package cl.ucn.disc.hpc.charlie.multithreadsudoku;
 
-public class App {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
-    public static void main(String[] args) {
-        SudokuGrid sudokuGrid = new SudokuGrid(9);
+public class InputFile {
 
-        sudokuGrid.printSudoku();
-
-        InputFile inputFile = new InputFile();
-        inputFile.readFile();
+    public InputFile() {
 
     }
 
+    public void readFile() {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File("sudoku9x9.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null)
+                System.out.println(linea);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
 }
